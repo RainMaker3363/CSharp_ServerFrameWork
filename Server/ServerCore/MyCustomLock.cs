7acff88d109eae4b7cf531c6ff9fcf962e183b16
RainMaker3363 <jsc22637@gmail.com>
2020-10-04 17:54:07 +0900

    My Work 1
    멀티 쓰레드 및 Lock 기법 연습

---
+++ Server/ServerCore/MyCustomLock.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace ServerCore
+{
+    // 재귀적 락을 허용할지 (Yes)
+    // 스핀락 정책 (5000번 -> Yield)
+    class MyCustomLock
+    {
+        const int EMPTY_FLAG = 0x00000000;
+        const int WRITE_MASK = 0x7FFF0000;
+        const int READ_MASK = 0x0000FFFF;
+        const int MAX_SPIN_COUNT = 5000;
+
+        // [unused(1)] [WriteThreadID(15)] [ReadCount(16)]
+        int _flag = EMPTY_FLAG;
+        int _writeCount = 0;
+
+        public void WriteLock()
+        {
+            int _lockThreadId = (_flag & WRITE_MASK) >> 16;
+            if(Thread.CurrentThread.ManagedThreadId == _lockThreadId)
+            {
+                ++_writeCount;
+                return;
+            }
+
+            // 아무도 WriteLock or ReadLock을 획득하고 있지 않을 때, 경합해서 소유권을 얻는다
+            int desired = (Thread.CurrentThread.ManagedThreadId << 16) & WRITE_MASK;
+
+            while(true)
+            {
+                for(int i = 0; i<MAX_SPIN_COUNT; ++i)
+                {
+                    // 원자성 보장을 위해 InterLock으로 체크합니다.
+                    // 반환 값은 바꾸기 이전 값!
+                    if (Interlocked.CompareExchange(ref _flag, desired, EMPTY_FLAG) == EMPTY_FLAG)
+                    {
+                        _writeCount = 1;
+                        return;
+                    }
+                }
+
+                Thread.Yield();
+            }
+        }
+
+        public void WriteUnLock()
+        {
+            int lockCount = --_writeCount;
+            if(lockCount == 0)
+                Interlocked.Exchange(ref _flag, EMPTY_FLAG);
+        }
+
+        public void ReadLock()
+        {
+            int _lockThreadId = (_flag & WRITE_MASK) >> 16;
+            if (Thread.CurrentThread.ManagedThreadId == _lockThreadId)
+            {
+                Interlocked.Increment(ref _flag);
+                return;
+            }
+
+            // 아무도 WriteLock을 획득하지 않았다면 ReadCount를 늘린다.
+            while (true)
+            {
+                for (int i = 0; i < MAX_SPIN_COUNT; ++i)
+                {
+                    int expected = (_flag & READ_MASK);
+                    if (Interlocked.CompareExchange(ref _flag, expected + 1, expected) == expected)
+                        return;
+                }
+
+                Thread.Yield();
+            }
+        }
+
+        public void ReadUnLock()
+        {
+            Interlocked.Decrement(ref _flag);
+        }
+    }
+}
---
+++ Server/ServerCore/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ServerCore
+{
+    #region SpinLock
+
+    class CustomSpinLock
+    {
+        volatile int _locked = 0;
+
+        public void Acquire()
+        {
+            while(true)
+            {
+                //int Original = Interlocked.Exchange(ref _locked, 1);
+                //if (Original == 0)
+                //    break;
+                int expected = 0;
+                int desired = 1;
+
+                // CAS Compared and Swap
+                int Original = Interlocked.CompareExchange(ref _locked, desired, expected);
+                if (Original == 0)
+                    break;
+
+                // 쉬다 올게! (컨텐스트 스위칭!)
+
+            }
+            
+        }
+
+        public void Release()
+        {
+            _locked = 0;
+        }
+    }
+
+    #endregion
+
+    #region 커널 Lock
+
+    class Lock
+    {
+        // bool <- 커널
+        AutoResetEvent _available = new AutoResetEvent(true);
+        //ManualResetEvent _available = new ManualResetEvent(true);
+
+        public void Acquire()
+        {
+            // 입장 시도
+            _available.WaitOne();
+            
+            // 문을 닫는다
+            //_available.Reset();
+        }
+
+        public void Release()
+        {
+            _available.Set();
+        }
+    }
+    #endregion
+
+    class Program
+    {
+        static int _num = 0;
+        //static Mutex _lock = new Mutex();
+        static SpinLock _lock2 = new SpinLock();
+
+        // 상호 베타
+        // Monitor
+        static object _lock = new object();
+
+        static ReaderWriterLockSlim _lock3 = new ReaderWriterLockSlim();
+        static MyCustomLock _lock4 = new MyCustomLock();
+
+        static ThreadLocal<string> _threadName = new ThreadLocal<string>();
+
+        static void Thread_1()
+        {
+            for(int i = 0; i<10000; ++i)
+            {
+                _lock4.WriteLock();
+                ++_num;
+                _lock4.WriteUnLock();
+            }
+        }
+
+        static void Thread_2()
+        {
+            for (int i = 0; i < 10000; ++i)
+            {
+                _lock4.WriteLock();
+                --_num;
+                _lock4.WriteUnLock();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Task t1 = new Task(Thread_1);
+            Task t2 = new Task(Thread_2);
+
+            t1.Start();
+            t2.Start();
+
+            Task.WaitAll(t1, t2);
+
+            Console.WriteLine($"Number : {_num}");
+        }
+    }
+}
